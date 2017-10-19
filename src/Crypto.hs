41ce5beb66a268dda6df829cf18278f04432fa4d
AbrahamAlcaina <abraham@weareadaptive.com>
2017-10-19 17:02:21 +0200

    test(crypto): add more test

--- src/Crypto.hs
+++ src/Crypto.hs
@@ -1,23 +1,26 @@
-module Crypto (createKeys, hashMsg, sign, signBigMsg) where
+module Crypto (createKeys, hashMsg, signSmallMsg, signBigMsg) where
 
+import           Crypto.Error
 import           Crypto.Hash            (Digest, hash)
 import           Crypto.Hash.Algorithms (SHA3_512)
 import           Crypto.PubKey.Ed25519  (PublicKey, SecretKey, Signature, generateSecretKey, sign,
                                          toPublic)
+
 import qualified Data.ByteString        as BS
-import qualified Data.ByteString.Char8  as C8
+import qualified Data.ByteString.Char8  as C
 
+type HashAlgoritm = Digest SHA3_512
 
-createKeys::IO (PublicKey, SecretKey)
+createKeys :: IO (PublicKey, SecretKey)
 createKeys = do
     sk <- generateSecretKey
     return (toPublic sk, sk)
 
 hashMsg::BS.ByteString -> String
-hashMsg bs = show (hash bs :: Digest SHA3_512)
+hashMsg bs = show (hash bs :: HashAlgoritm)
 
 signSmallMsg:: SecretKey -> PublicKey -> BS.ByteString  -> Signature
 signSmallMsg = sign
 
 signBigMsg:: SecretKey -> PublicKey -> BS.ByteString -> Signature
-signBigMsg sk pk msg = signSmallMsg sk pk  $ C8.pack (hashMsg msg)
+signBigMsg sk pk msg = signSmallMsg sk pk  $ C.pack (hashMsg msg)
--- test/CryptoSpec.hs
+++ test/CryptoSpec.hs
@@ -1,20 +1,35 @@
 module CryptoSpec (spec) where
 
 import           Crypto
+import           Crypto.Error
+import           Crypto.PubKey.Ed25519 (sign)
 import qualified Data.ByteString       as BS
 import qualified Data.ByteString.Char8 as C
 import           SpecHelper
 
 spec::Spec
-spec = describe "Crypto fixture" $ do
-    it "Should Sign" $
-        getByteStream `shouldBe` getHash
+spec = describe "Feature: Cryptography" $ do
+    describe "Scenario: User wants to get a hash of a message" $
+        describe "Given I have a message `hola`" $
+            it ("Then the hash is " ++ getHash) $
+                getByteStream `shouldBe` getHash
+    describe "Scenario: User wants to sign a message" $
+        describe "Given When I have private and publick key" $ do
+            describe "When I have two different messages " $
+                it "Then I should have differents signatures" $ do
+                    (pk, sk) <- createKeys
+                    signSmallMsg sk pk msgBS `shouldNotBe` sign sk pk (C.pack "no")
+            describe "When I have two equal messages" $
+                it "Then I should have the same signature" $ do
+                    (pk, sk) <- createKeys
+                    signSmallMsg sk pk msgBS `shouldBe` sign sk pk msgBS
     {- generate private & public key
     it "should generate keys" $ do
         (pk, sk) <- createKeys
         BS.writeFile "./test/utils/sk" (unSecretKey sk)
         BS.writeFile "./test/utils/pk" (unPublicKey pk)
     -}
-getByteStream = hashMsg $ C.pack "hola"
+msg = "hola"
+msgBS = C.pack msg
+getByteStream = hashMsg msgBS
 getHash = "42a46be94ae2242bdc13620cb065e64391059b8f909e27208293f462f3dd51669811446b3f8227c72bf6a1cf2628584429578b5d41756fbd02b40b45bfae8faf"
-
